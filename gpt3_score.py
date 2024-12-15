03b241dc74b75108727b6909a2d443a242cfe883
Kevin Toh <kevintoh0305@gmail.com>
2024-12-15 12:29:46 +0800

    Update gpt3_score to use the latest openai interfaces

--- gpt3_score.py
+++ gpt3_score.py
@@ -1,20 +1,18 @@
 from gpt_inference import GPT3Model
 
-def gpt3score(input, output,gpt3model=None,api_key=None):
+def gpt3score(input, output, gpt3model=None, api_key=None):
+    """
+    GPT-3 Models
+    "text-curie-001", "text-ada-001", "text-babbage-001", "text-davinci-001", "text-davinci-003"
+    """
+    
     gpt3model_name = ''
-    if gpt3model == 'ada':
-        gpt3model_name = "text-ada-001"
-    elif gpt3model == 'babbage':
-        gpt3model_name = "text-babbage-001"
-    elif gpt3model == 'curie':
-        gpt3model_name = "text-curie-001"
-    elif gpt3model == 'davinci001':
-        gpt3model_name = "text-davinci-001"
-    elif gpt3model == 'davinci003':
-        gpt3model_name = "text-davinci-003"
-    print('gpt3model_name: ', gpt3model_name)
-
-    # "text-curie-001", "text-ada-001", "text-babbage-001", "text-davinci-001", "text-davinci-003",
+    if gpt3model in ['ada', 'babbage']:
+        gpt3model_name = "babbage-002"
+    elif gpt3model in ['curie', 'davinci001', 'davinci-003']:
+        gpt3model_name = "davinci-002"
+    print('gpt_model_name: ', gpt3model_name)
+    
     metaicl_model = GPT3Model(gpt3model_name, api_key)
     avg_loss = metaicl_model.do_inference(input, output)
     score = -avg_loss
--- gpt_inference.py
+++ gpt_inference.py
@@ -18,43 +18,44 @@ class GPT3Model(object):
         losses = []
         data = input + output
 
-        response = self.gpt3(data)
-        out = response["choices"][0]
+        response = self.gpt(data)
+        print(response)
+        out = response.choices[0]
 
-        assert input + output == out["text"]
+        assert input + output == out.text
+        
         i = 0
         # find the end position of the input...
-        i = out['logprobs']['text_offset'].index(len(input) - 1)
+        i = out.logprobs.text_offset.index(len(input))
         if i == 0:
             i = i + 1
 
-        print('eval text', out['logprobs']['tokens'][i: -1])
-        loss = -sum(out['logprobs']["token_logprobs"][i:-1]) # ignore the last '.'
-        avg_loss = loss / (len(out['logprobs']['text_offset']) - i-1) # 1 is the last '.'
+        print('eval text', out.logprobs.tokens[i:-1])
+        loss = -sum(out.logprobs.token_logprobs[i:-1]) # ignore the last '.'
+        avg_loss = loss / (len(out.logprobs.text_offset) - i-1) # 1 is the last '.'
         print('avg_loss: ', avg_loss)
         losses.append(avg_loss)
 
         return avg_loss
 
-
-    def gpt3(self, prompt, max_len=0, temp=0, num_log_probs=0, echo=True, n=None):
+    def gpt(self, prompt, max_len=0, temp=0, num_log_probs=0, echo=True, n=None):
         response = None
         received = False
         while not received:
             try:
-                response = openai.Completion.create(engine=self.model_name,
-                                                    prompt=prompt,
-                                                    max_tokens=max_len,
-                                                    temperature=temp,
-                                                    logprobs=num_log_probs,
-                                                    echo=echo,
-                                                    stop='\n',
-                                                    n=n)
-                print('prompt: ',prompt)
+                response = openai.completions.create(model=self.model_name,
+                                                     prompt=prompt,
+                                                     max_tokens=max_len,
+                                                     temperature=temp,
+                                                     logprobs=num_log_probs,
+                                                     echo=echo,
+                                                     stop='\n',
+                                                     n=n)
+                print('prompt: ', prompt)
                 received = True
             except:
                 error = sys.exc_info()[0]
-                if error == openai.error.InvalidRequestError:
+                if error == openai.APIError:
                     # something is wrong: e.g. prompt too long
                     print(f"InvalidRequestError\nPrompt passed in:\n\n{prompt}\n\n")
                     assert False
